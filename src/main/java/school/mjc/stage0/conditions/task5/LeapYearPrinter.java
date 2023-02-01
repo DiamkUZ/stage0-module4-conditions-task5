23c45fdc7aac8419d3b86bc26bd980a1c4a8f8b6
DiamkUZ <diamkms@yandex.ru>
2023-02-01 15:27:25 +0300

    All done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+
+        if (year % 400 == 0) {
+            System.out.println("leap");
+        } else {
+            if (year % 100 == 0) {
+                System.out.println("not leap");
+            } else if (year % 4 == 0) {
+                System.out.println("leap");
+                
+            } else {
+                System.out.println("not leap");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter leapPrinter = new LeapYearPrinter();
+
+        leapPrinter.isLeapYear(1900);
     }
 }
